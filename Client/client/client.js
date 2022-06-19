adc9f5159df11dc4eb6686f10f072f3bb12b93d3
João <joaos_vb@hotmail.com>
2022-06-19 18:49:10 -0300

    Primeiro commit

---
+++ Client/client/client.js
@@ -0,0 +1,48 @@
+var PROTO_PATH = __dirname + '/../protos/convertwordtopdf.proto';
+
+var fs = require("fs");
+
+var grpc = require('@grpc/grpc-js');
+var protoLoader = require('@grpc/proto-loader');
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {keepCase: true,
+     longs: String,
+     enums: String,
+     defaults: true,
+     oneofs: true
+    });
+var converter = grpc.loadPackageDefinition(packageDefinition).ConvertDoc;
+
+var client = new converter.ConvertDocxToPdfService('localhost:50051', grpc.credentials.createInsecure());
+
+function sendDocxConvert(nameFile) {
+  const DocxConverter = client.ConvertDocxToPdf();
+
+  const readStream = fs.createReadStream(`${__dirname}/../tmp/${nameFile}`);
+        
+  readStream
+    .on("data", chunk => DocxConverter.write({chunk}))
+    .on("end", () => DocxConverter.end());
+
+  DocxConverter
+    .on("data", async ({ chunk }) => fs.appendFileSync(`${__dirname}/../tmp/${nameFile.replace( /docx$/, "pdf")}`, chunk));
+}
+
+function main() {
+  fs.readdir(`${__dirname}/../tmp/`, async (err, files) => {
+    if(files.length === 0 ){
+      console.log(`Arquivos .docx inexistentes em: ${__dirname.replace("client", "tmp")}`);
+    }
+
+    const docsToConvert = await files.filter(el => /docx$/.test(el));
+    
+    docsToConvert.map(async el => {
+      await sendDocxConvert(el);
+    });
+  });
+}
+
+main();
+
+exports.sendDocxConvert = sendDocxConvert;
---
+++ Server/service/server.js
@@ -0,0 +1,50 @@
+var PROTO_PATH = __dirname + '/../protos/convertwordtopdf.proto';
+
+var fs = require("fs");
+var grpc = require('@grpc/grpc-js');
+var protoLoader = require('@grpc/proto-loader');
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true
+    });
+ var ConvertDoc = grpc.loadPackageDefinition(packageDefinition).ConvertDoc;
+
+var docxConverter = require('docx-pdf');
+const { v4 } = require("uuid");
+
+function pathToPdf(call) {
+    let fileName = v4();
+
+    call.on("data", async ({ chunk }) => {
+        if(chunk) {
+            fs.appendFileSync(`${__dirname}/../tmp/${fileName}.docx`, chunk);
+        } 
+    });
+
+    call.on("end", async () => {
+        await docxConverter(`${__dirname}/../tmp/${fileName}.docx`, `${__dirname}/../tmp/${fileName}.pdf`, (err, result) => {
+            if(err) console.log(err);
+            
+            const readStream = fs.createReadStream(`${__dirname}/../tmp/${fileName}.pdf`);
+            readStream
+                .on("data", chunk => call.write({chunk}))
+                .on("end", () => call.end());
+            fileName = v4();
+        });
+    });
+}
+
+function main() {
+    var server = new grpc.Server();
+    server.addService(ConvertDoc.ConvertDocxToPdfService.service, { ConvertDocxToPdf: pathToPdf });
+    server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
+        server.start();
+    });
+}
+
+main();
